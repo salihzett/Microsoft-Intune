8b5bf788ea4d7d01949cec561d7d8953a6f89b6d
salihzett <salihzett@users.noreply.github.com>
2023-01-07 18:51:03 +0100

    Create hwid.ps1

---
+++ Windows/Autopilot/hwid.ps1
@@ -0,0 +1,8 @@
+# Script to gather the hashID for Autopilot. (legacy version)
+
+md c:\\HWID
+Set-Location c:\\HWID
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
+Install-Script -Name Get-WindowsAutoPilotInfo 
+$b = hostname
+Get-WindowsAutoPilotInfo.ps1 -OutputFile c:\$b.csv
