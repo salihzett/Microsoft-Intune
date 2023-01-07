78dd6d558e842a9a63c05118c85144b7461477da
salihzett <salihzett@users.noreply.github.com>
2023-01-07 18:42:00 +0100

    Create fetchMSTeamsVersion.sh

---
+++ macOS/Custom attributes/fetchMSTeamsVersion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#set -x
+
+############################################################################################
+##
+## Extension Attribute script to return the version of an installed App
+##
+############################################################################################
+
+# User Defined variables
+app="Microsoft Teams.app"
+
+# Fixed Variables
+attribute="CFBundleShortVersionString"
+InfoPlistPath="/Applications/$app/Contents/Info.plist"
+
+# Attempt to read CFBundleStringShortVersionString and return it
+if [[ -f "$InfoPlistPath" ]]; then
+ver=$(plutil -p "$InfoPlistPath" | grep "$attribute" | awk -F'"' '{ print $4 }')
+    echo $ver
+else
+    echo "not installed"
+fi
