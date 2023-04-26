c0e3e7c5cd7b083ef32ad3505348882995280873
salihzett <salihzett@users.noreply.github.com>
2023-04-26 20:41:51 +0200

    Update installOneDriveFinderIntegration.sh

--- macOS/Shell scripts/installOneDriveFinderIntegration.sh
+++ macOS/Shell scripts/installOneDriveFinderIntegration.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-echo "$(date) | Setup OneDrive FinderAddon"
+echo ""
+echo "##############################################################"
+echo "# $(date) | Setup OneDrive FinderAddon"
+echo "############################################################"
+echo ""
 
 /usr/bin/pluginkit -e use -i com.microsoft.OneDrive-mac.FileProvider
 /usr/bin/pluginkit -e use -i com.microsoft.OneDrive-mac.FinderSync
